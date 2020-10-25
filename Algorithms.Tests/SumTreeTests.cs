8e08ef2190d0bc24c07c60e1c0d9e970cad73cb2
Vadim Formanyuk <vformanyuk@gmail.com>
2020-10-25 14:54:29 +0300

    sum tree

---
+++ Algorithms.Tests/SumTreeTests.cs
@@ -0,0 +1,77 @@
+﻿using Algorithms.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class SumTreeTests
+    {
+        private SumTree _testSubject;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _testSubject = new SumTree(capacity: 8);
+
+            _testSubject.Add(9);
+            _testSubject.Add(13);
+            _testSubject.Add(1);
+            _testSubject.Add(3);
+            _testSubject.Add(11);
+            _testSubject.Add(18);
+            _testSubject.Add(5);
+            _testSubject.Add(8);
+
+            Console.WriteLine(_testSubject.ToString());
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _testSubject = null;
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(68, _testSubject.Total);
+        }
+
+        [TestMethod]
+        public void TestAddOverlap()
+        {
+            _testSubject.Add(10);
+            Assert.AreEqual(69, _testSubject.Total);
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            _testSubject.Delete(0);
+            Assert.AreEqual(59, _testSubject.Total);
+        }
+
+        [TestMethod]
+        public void TestUpdate()
+        {
+            _testSubject.Update(11, 1);
+            Assert.AreEqual(66, _testSubject.Total);
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 9, DisplayName = "Lower bound")]
+        [DataRow(67, 8, DisplayName = "Upper bound")]
+        [DataRow(22, 1, DisplayName = "Single")]
+        [DataRow(54, 18, DisplayName = "Transition 1")]
+        [DataRow(55, 5, DisplayName = "Transition 1")]
+        [DataRow(56, 5, DisplayName = "Transition 1")]
+        public void TestGet(int input, int expected)
+        {
+            var result = _testSubject.Get(input);
+            Assert.AreEqual(expected, result);
+        }
+    } 
+}
---
+++ Algorithms/DataStructures/SumTree.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithms.DataStructures
+{
+    public class SumTree
+    {
+        private int _writeIdx;
+        private int[] _tree;
+
+        public SumTree(int capacity)
+        {
+            if (capacity % 2 == 1)
+            {
+                capacity++;
+            }
+            Capacity = capacity;
+            _tree = new int[capacity * 2 - 1];
+        }
+
+        public int Capacity { get; }
+
+        public int Total => _tree[_tree.Length - 1];
+
+        public int Add(int item)
+        {
+            _tree[_writeIdx] = item;
+
+            Propagate(_writeIdx);
+
+            var oldIdx = _writeIdx;
+
+            _writeIdx = ++_writeIdx % Capacity;
+
+            return oldIdx;
+        }
+
+        public void Update(int item, int idx)
+        {
+            _tree[idx] = item;
+
+            Propagate(idx);
+        }
+
+        public void Delete(int idx)
+        {
+            _tree[idx] = 0;
+            Propagate(idx);
+        }
+
+        public int Get(int search)
+        {
+            int lvlIdx = _tree.Length - 1;
+            if (_tree[lvlIdx] < search)
+            {
+                throw new ArgumentOutOfRangeException("search", "Total tree sum is lesser then requested number");
+            }
+
+
+            int offset = 0;
+            int searchIdx = lvlIdx;
+            int lvl = 1;
+
+            do
+            {
+                lvlIdx -= (int)Math.Pow(2, lvl);
+
+                int leftChildIdx = lvlIdx + offset * 2;
+                int rightChildIdx = leftChildIdx + 1;
+
+                if (_tree[leftChildIdx] > search)
+                {
+                    searchIdx = leftChildIdx;
+                }
+                else
+                {
+                    searchIdx = rightChildIdx;
+                    search -= _tree[leftChildIdx];
+                }
+
+                if (lvlIdx > 0)
+                {
+                    offset = searchIdx % lvlIdx;
+                }
+                lvl++;
+            }
+            while (lvlIdx > 0);
+
+            return _tree[searchIdx];
+        }
+
+        private void Propagate(int idx, int lvl = 1)
+        {
+            if (idx >= _tree.Length - 1)
+            {
+                return;
+            }
+
+            int coupleIdx = idx;
+            int offset = 0;
+
+            if (idx % 2 == 1)
+            {
+                coupleIdx--;
+                offset = -1;
+            }
+            else
+            {
+                coupleIdx++;
+            }
+
+            int parentIdx = Capacity / lvl + (idx + offset) / 2;
+            _tree[parentIdx] = _tree[idx] + _tree[coupleIdx];
+            Propagate(parentIdx, lvl++);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder rep = new StringBuilder();
+            int end = Capacity;
+            int offset = 0;
+            while (end > 0)
+            {
+                int startInterval = 0;
+
+                for (int i = 0; i < end; i++)
+                {
+                    rep.AppendFormat("[{0}]({1} - {2})\t", _tree[offset + i], startInterval, startInterval + _tree[offset + i] - 1);
+                    startInterval += _tree[offset + i];
+                }
+                rep.AppendLine();
+                offset += end;
+                end /= 2;
+            }
+            return rep.ToString();
+        }
+    }
+}
